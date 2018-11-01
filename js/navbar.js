da4025a7a355911c2447cd047bef0c701c555381
Connie Luong <col032@ucsd.edu>
2018-10-31 18:18:03 -0700

    added navbar to all pages (Connie)

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	loadNavBar();
+})
+
+/*
+ * Function that is called when the document is ready.
+ * Loads navbar
+ */
+function loadNavBar() {
+	$("#navbar").html(
+        '<ul>'+
+        '<li id="my-account"><a href="../my-account.html">My Account</a></li>'+
+        '<li id="explore"><a href="../index.html">Explore</a></li>'+
+        '<li id="search"><a href="../search.html">Search</a></li>'+
+        '<li id="more"><a href="../more.html">More</a></li>'+'</ul>'
+    );
+
+}
