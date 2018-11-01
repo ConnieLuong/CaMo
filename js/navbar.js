4562c771e89aeacb684362de93e35d41518ee267
Connie Luong <connie@Connies-iMac.local>
2018-11-01 12:47:57 -0700

    added new navbar to all pages (CL)

--- js/navbar.js
+++ js/navbar.js
@@ -6,6 +6,28 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  * Loads navbar
  */
+function loadNavBar(){
+    $("#navbar").html(
+        '<table cellspacing="0" cellpadding="0">'+
+            '<tbody>'+
+            '<tr>'+
+                '<td class="icon profile"><a href="../profile.html"><img src="../image/icons8-user.png" alt="profile icon" width="70%"></a></td>'+
+                '<td class="icon explore"><a href="../index.html"><img src="../image/icons8-hot_chocolate.png" alt="explore icon" width="70%"></a></td>'+
+                '<td class="icon search"><a href="../search.html"><img src="../image/icons8-search.png" alt="search icon" width="70%"></a></td>'+
+                '<td class="icon more"><a href="../more.html"><img src="../image/icons8-more.png" alt="more icon" width="70%"></a></td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td class="profile"><a href="../profile.html" style="color: #FFFCFC;">Profile</a></td>'+
+                '<td class="explore"><a href="../index.html" style="color: #FFFCFC;">Explore</a></td>'+
+                '<td class="search"><a href="../search.html" style="color: #FFFCFC;">Search</a></td>'+
+                '<td class="more"><a href="../more.html" style="color: #FFFCFC;">More</a></td>'+
+            '</tr>'+
+            '</tbody>'+
+        '</table>'
+    );
+}
+
+/* Old navbar js loader
 function loadNavBar() {
 	$("#navbar").html(
         '<ul>'+
@@ -15,4 +37,4 @@ function loadNavBar() {
         '<li id="more"><a href="../more.html">More</a></li>'+'</ul>'
     );
 
-}
+}*/
