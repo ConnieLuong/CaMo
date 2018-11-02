9eb2a73ea342660a5609be5367f9f2b64e9e392a
Connie Luong <col032@ucsd.edu>
2018-11-01 20:30:04 -0700

    added slideshow

--- js/mainpages.js
+++ js/mainpages.js
@@ -11,7 +11,7 @@ function loadNavBar(){
 
         '<ul class="menuopt">'+
         '<a href="../index.html"><img src="image/LightLogoHor.png" id="logo" alt="profile icon" width="100"></a>'+
-        '<li><a href="more.html">About</a></li>'+
+        '<li><a href="more.html">More</a></li>'+
         '<li><a href="search.html">Search</a></li>'+
         '<li><a href="profile.html">Profile</a></li>'+
         '<li><a href="index.html">Explore</a></li>'+
