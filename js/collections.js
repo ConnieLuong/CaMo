ceb671519e134f2c448b383b99ee119f65f9ff53
Hongrui Zhu <hoz046@ucsd.edu>
2018-11-08 19:49:38 -0800

    categories

--- js/collections.js
+++ js/collections.js
@@ -23,7 +23,7 @@ var collectionData =[
         'cafe3-hashtags':'#quiet',
         'cafe3-link':'cafe_page=The%20Split%20Bean',
     
-        'cafe4-img':'Cafe10.png',
+        'cafe4-img':'Cafe3.png',
         'cafe4-name':'The Grind',
         'cafe4-rating':'2/5',
         'cafe4-hashtags':'#quiet',
@@ -35,7 +35,7 @@ var collectionData =[
         'cafe5-hashtags':'#quiet',
         'cafe5-link':'cafe_page=Cute%20Cups',
     
-        'cafe6-img':'Cafe2.png',
+        'cafe6-img':'Cafe9.png',
         'cafe6-name':'Black Sugars',
         'cafe6-rating':'4/5',
         'cafe6-hashtags':'#quiet',
@@ -47,7 +47,7 @@ var collectionData =[
         'cafe7-hashtags':'#quiet',
         'cafe7-link':'cafe_page=No%20Doze%20Cafe',
     
-        'cafe8-img':'Cafe8.png',
+        'cafe8-img':'Cafe6.png',
         'cafe8-name':'Aroma Mocha',
         'cafe8-rating':'4.5/5',
         'cafe8-hashtags':'#quiet',
@@ -59,158 +59,158 @@ var collectionData =[
         'header_img':'../image/wish-list.jpg', 
         
         'cafe1-img':'Cafe13.png',
-        'cafe1-name':'The Bean Palace',
+        'cafe1-name':'Amooze',
         'cafe1-rating':'3/5',
         'cafe1-hashtags':'#twentyfourh',
-        'cafe1-link':'cafe_page=The%20Bean%20Palace',
+        'cafe1-link':'cafe_page=Amooze',
     
-        'cafe2-img':'Cafe9.png',
+        'cafe2-img':'Cafe2.png',
         'cafe2-name':'Lava Java',
         'cafe2-rating':'3.5/5',
         'cafe2-hashtags':'#twentyfourh',
         'cafe2-link':'cafe_page=Lava%20Java',
     
-        'cafe3-img':'Cafe8.png',
-        'cafe3-name':'The Busy Bean',
+        'cafe3-img':'Cafe14.png',
+        'cafe3-name':'CHao',
         'cafe3-rating':'2/5',
         'cafe3-hashtags':'#twentyfourh',
-        'cafe3-link':'cafe_page=The%20Busy%20Bean',
+        'cafe3-link':'cafe_page=CHao',
     
-        'cafe4-img':'Cafe1.png',
+        'cafe4-img':'Cafe12.png',
         'cafe4-name':'Cute Cups',
         'cafe4-rating':'1/5',
         'cafe4-hashtags':'#twentyfourh',
         'cafe4-link':'cafe_page=Cute%20Cups',
     
-        'cafe5-img':'Cafe9.png',
+        'cafe5-img':'Cafe5.png',
         'cafe5-name':'No Doze Cafe',
         'cafe5-rating':'4/5',
         'cafe5-hashtags':'#twentyfourh',
         'cafe5-link':'cafe_page=No%20Doze%20Cafe',
     
-        'cafe6-img':'Cafe8.png',
+        'cafe6-img':'Cafe3.png',
         'cafe6-name':'The Grind',
         'cafe6-rating':'3/5',
         'cafe6-hashtags':'#twentyfourh',
         'cafe6-link':'cafe_page=The%20Grind',
     
-        'cafe7-img':'Cafe6.png',
-        'cafe7-name':'Aroma Mocha',
+        'cafe7-img':'Cafe10.png',
+        'cafe7-name':'Bees',
         'cafe7-rating':'3.5/5',
         'cafe7-hashtags':'#twentyfourh',
-        'cafe7-link':'cafe_page=Aroma%20Mocha',
+        'cafe7-link':'cafe_page=Bees',
     
         'cafe8-img':'Cafe15.png',
-        'cafe8-name':'Black Sugars',
+        'cafe8-name':'Merp',
         'cafe8-rating':'1/5',
         'cafe8-hashtags':'#twentyfourh',
-        'cafe8-link':'cafe_page=Black%20Sugars'
+        'cafe8-link':'cafe_page=Merp'
     },
     //collection3
     {
         'collection_name':'cool af', 
         'header_img':'../image/cool-af.jpg', 
         
-        'cafe1-img':'Cafe14.png',
-        'cafe1-name':'The Bean Palace',
-        'cafe1-rating':'3/5',
-        'cafe1-hashtags':'#twentyfourh',
-        'cafe1-link':'cafe_page=The%20Bean%20Palace',
-    
-        'cafe2-img':'Cafe10.png',
-        'cafe2-name':'Lava Java',
-        'cafe2-rating':'3.5/5',
-        'cafe2-hashtags':'#twentyfourh',
-        'cafe2-link':'cafe_page=Lava%20Java',
-    
-        'cafe3-img':'Cafe6.png',
-        'cafe3-name':'The Grind',
-        'cafe3-rating':'3/5',
-        'cafe3-hashtags':'#twentyfourh',
-        'cafe3-link':'cafe_page=The%20Grind',
-    
-        'cafe4-img':'Cafe5.png',
-        'cafe4-name':'The Busy Bean',
-        'cafe4-rating':'4/5',
-        'cafe4-hashtags':'#twentyfourh',
-        'cafe4-link':'cafe_page=The%20Busy%20Bean',
-    
-        'cafe5-img':'Cafe3.png',
-        'cafe5-name':'Cute Cups',
-        'cafe5-rating':'1/5',
-        'cafe5-hashtags':'#twentyfourh',
-        'cafe5-link':'cafe_page=Cute%20Cups',
-    
-        'cafe6-img':'Cafe4.png',
-        'cafe6-name':'No Doze Cafe',
-        'cafe6-rating':'3.5/5',
-        'cafe6-hashtags':'#twentyfourh',
-        'cafe6-link':'cafe_page=No%20Doze%20Cafe',
-    
-        'cafe7-img':'Cafe12.png',
-        'cafe7-name':'Aroma Mocha',
-        'cafe7-rating':'3/5',
-        'cafe7-hashtags':'#twentyfourh',
-        'cafe7-link':'cafe_page=Aroma%20Mocha',
-    
-        'cafe8-img':'Cafe1.png',
-        'cafe8-name':'Black Sugars',
-        'cafe8-rating':'2/5',
-        'cafe8-hashtags':'#twentyfourh',
-        'cafe8-link':'cafe_page=Black%20Sugars' 
+        'cafe1-img':'Cafe16.png',
+    'cafe1-name':'GRE',
+    'cafe1-rating':'1/5',
+    'cafe1-hashtags':'#aesthetic',
+    'cafe1-link':'cafe_page=GRE',
+
+    'cafe2-img':'Cafe2.png',
+    'cafe2-name':'Lava Java',
+    'cafe2-rating':'2/5',
+    'cafe2-hashtags':'#aesthetic',
+    'cafe2-link':'cafe_page=Lava%20Java',
+
+    'cafe3-img':'Cafe5.png',
+    'cafe3-name':'No Doze Cafe',
+    'cafe3-rating':'4/5',
+    'cafe3-hashtags':'#aesthetic',
+    'cafe3-link':'cafe_page=No%20Doze%20Cafe',
+
+    'cafe4-img':'Cafe15.png',
+    'cafe4-name':'Merp',
+    'cafe4-rating':'4.5/5',
+    'cafe4-hashtags':'#aesthetic',
+    'cafe4-link':'cafe_page=Merp',
+
+    'cafe5-img':'Cafe12.png',
+    'cafe5-name':'Cute Cups',
+    'cafe5-rating':'3/5',
+    'cafe5-hashtags':'#aesthetic',
+    'cafe5-link':'cafe_page=Cute%20Cups',
+
+    'cafe6-img':'Cafe3.png',
+    'cafe6-name':'The Grind',
+    'cafe6-rating':'3.5/5',
+    'cafe6-hashtags':'#aesthetic',
+    'cafe6-link':'cafe_page=The%20Grind',
+
+    'cafe7-img':'Cafe6.png',
+    'cafe7-name':'Aroma Mocha',
+    'cafe7-rating':'4.5/5',
+    'cafe7-hashtags':'#aesthetic',
+    'cafe7-link':'cafe_page=Aroma%20Mocha',
+
+    'cafe8-img':'Cafe9.png',
+    'cafe8-name':'Black Sugars',
+    'cafe8-rating':'3/5',
+    'cafe8-hashtags':'#aesthetic',
+    'cafe8-link':'cafe_page=Black%20Sugars'  
     },
     //collection4
     {
         'collection_name':'moms favs',
         'header_img':'../image/momsfaves.png', 
         
-        'cafe1-img':'Cafe16.png',
+        'cafe1-img':'Cafe12.png',
         'cafe1-name':'Cute Cups',
         'cafe1-rating':'3/5',
         'cafe1-hashtags':'#vegan',
         'cafe1-link':'cafe_page=Cute%20Cups',
-
-        'cafe2-img':'Cafe13.png',
+    
+        'cafe2-img':'Cafe2.png',
         'cafe2-name':'Lava Java',
         'cafe2-rating':'4/5',
         'cafe2-hashtags':'#vegan',
         'cafe2-link':'cafe_page=Lava%20Java',
-
+    
         'cafe3-img':'Cafe1.png',
         'cafe3-name':'The Bean Palace',
         'cafe3-rating':'3.5/5',
         'cafe3-hashtags':'#vegan',
         'cafe3-link':'cafe_page=The%20Bean%20Palace',
-
-        'cafe4-img':'Cafe1.png',
+    
+        'cafe4-img':'Cafe5.png',
         'cafe4-name':'No Doze Cafe',
         'cafe4-rating':'2/5',
         'cafe4-hashtags':'#vegan',
         'cafe4-link':'cafe_page=No%20Doze%20Cafe',
-
-        'cafe5-img':'Cafe9.png',
-        'cafe5-name':'The Busy Bean',
+    
+        'cafe5-img':'Cafe16.png',
+        'cafe5-name':'GRE',
         'cafe5-rating':'1/5',
         'cafe5-hashtags':'#vegan',
-        'cafe5-link':'cafe_page=The%20Busy%20Bean',
-
-        'cafe6-img':'Cafe7.png',
+        'cafe5-link':'cafe_page=GRE',
+    
+        'cafe6-img':'Cafe3.png',
         'cafe6-name':'The Grind',
         'cafe6-rating':'2/5',
         'cafe6-hashtags':'#vegan',
         'cafe6-link':'cafe_page=The%20Grind',
-
-        'cafe7-img':'Cafe5.png',
+    
+        'cafe7-img':'Cafe6.png',
         'cafe7-name':'Aroma Mocha',
         'cafe7-rating':'3/5',
         'cafe7-hashtags':'#vegan',
         'cafe7-link':'cafe_page=Aroma%20Mocha',
-
-        'cafe8-img':'Cafe3.png',
+    
+        'cafe8-img':'Cafe9.png',
         'cafe8-name':'Black Sugars',
         'cafe8-rating':'2/5',
         'cafe8-hashtags':'#vegan',
-        'cafe8-link':'cafe_page=Black%20Sugars'  
+        'cafe8-link':'cafe_page=Black%20Sugars' 
     }
    ]
     
