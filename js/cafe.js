33327ca886be595bb8b2c5a9fedd6fb73af14ace
Hongrui Zhu <hoz046@ucsd.edu>
2018-11-08 20:41:31 -0800

    cafe data

--- js/cafe.js
+++ js/cafe.js
@@ -2,226 +2,226 @@
 var cafeData =[
     //cafe1
     {
-        'cafe_name':'No_Social_Life', 
+        'cafe_name':'The Bean Palace', 
         'header_img':'../image/2.jpg', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'food/food1.png',
+        'food2-img':'food/food4.png',
+        'food3-img':'drink/drink10.png',
+        'food4-img':'drink/drink1.png',
+        'food5-img':'food/food19.png',
+        'food6-img':'drink/drink8.png',
+        'food7-img':'drink/drink3.png',
+        'food8-img':'food/food8.png'
 
     },
     //cafe2
     {
-        'cafe_name':'', 
+        'cafe_name':'Lava Java', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'food/food2.png',
+        'food2-img':'drink/drink2.png',
+        'food3-img':'drink/drink7.png',
+        'food4-img':'food/food7.png',
+        'food5-img':'drink/drink9.png',
+        'food6-img':'drink/drink5.png',
+        'food7-img':'food/food10.png',
+        'food8-img':'drink/drink3.png'
 
     },
     //cafe3
     {
-        'cafe_name':'', 
+        'cafe_name':'The Grind', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'food/food4.png',
+        'food2-img':'drink/drink10.png',
+        'food3-img':'drink/drink8.png',
+        'food4-img':'food/food5.png',
+        'food5-img':'food/food12.png',
+        'food6-img':'drink/drink6.png',
+        'food7-img':'food/food15.png',
+        'food8-img':'drink/drink2.png'
 
     }, 
     //cafe4
     {
-        'cafe_name':'', 
+        'cafe_name':'No Social Life', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'food/food17.png',
+        'food2-img':'drink/drink8.png',
+        'food3-img':'drink/drink5.png',
+        'food4-img':'food/food18.png',
+        'food5-img':'drink/drink3.png',
+        'food6-img':'food/food1.png',
+        'food7-img':'drink/drink2.png',
+        'food8-img':'drink/drink1.png'
 
     },
     //cafe5
     {
-        'cafe_name':'', 
+        'cafe_name':'No Dose Cafe', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink6.png',
+        'food2-img':'food/food7.png',
+        'food3-img':'drink/drink8.png',
+        'food4-img':'food/food2.png',
+        'food5-img':'drink/drink9.png',
+        'food6-img':'food/food5.png',
+        'food7-img':'drink/drink10.png',
+        'food8-img':'drink/drink1.png'
 
     },
     //cafe6
     {
-        'cafe_name':'', 
+        'cafe_name':'Aroma Mocha', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'food/food12.png',
+        'food2-img':'drink/drink3.png',
+        'food3-img':'drink/drink2.png',
+        'food4-img':'drink/drink8.png',
+        'food5-img':'food/food8.png',
+        'food6-img':'drink/drink5.png',
+        'food7-img':'food/food4.png',
+        'food8-img':'drink/drink1.png'
 
     },
     //cafe7
     {
-        'cafe_name':'', 
+        'cafe_name':'The Split Cafe', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'food/food2.png',
+        'food2-img':'drink/drink10.png',
+        'food3-img':'drink/drink8.png',
+        'food4-img':'food/food5.png',
+        'food5-img':'drink/drink6.png',
+        'food6-img':'drink/drink4.png',
+        'food7-img':'food/food6.png',
+        'food8-img':'food/food7.png'
 
     },
     //cafe8
     {
-        'cafe_name':'', 
+        'cafe_name':'Happy Orange', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink2.png',
+        'food2-img':'food/food8.png',
+        'food3-img':'food/food9.png',
+        'food4-img':'drink/drink9.png',
+        'food5-img':'drink/drink7.png',
+        'food6-img':'food/food12.png',
+        'food7-img':'food/food13.png',
+        'food8-img':'drink/drink5.png'
 
     },
     //cafe9
     {
-        'cafe_name':'', 
+        'cafe_name':'Black Sugars', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink3.png',
+        'food2-img':'food/food4.png',
+        'food3-img':'drink/drink1.png',
+        'food4-img':'food/food14.png',
+        'food5-img':'food/food18.png',
+        'food6-img':'drink/drink10.png',
+        'food7-img':'food/food19.png',
+        'food8-img':'drink/drink8.png'
 
     },
     //cafe10
     {
-        'cafe_name':'', 
+        'cafe_name':'Bees', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink6.png',
+        'food2-img':'drink/drink4.png',
+        'food3-img':'food/food1.png',
+        'food4-img':'drink/drink2.png',
+        'food5-img':'food/food2.png',
+        'food6-img':'food/food7.png',
+        'food7-img':'drink/drink9.png',
+        'food8-img':'food/food11.png'
 
     },
     //cafe11
     {
-        'cafe_name':'', 
+        'cafe_name':'QnA', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink7.png',
+        'food2-img':'food/food1.png',
+        'food3-img':'drink/drink5.png',
+        'food4-img':'food/food6.png',
+        'food5-img':'food/food8.png',
+        'food6-img':'drink/drink3.png',
+        'food7-img':'drink/drink1.png',
+        'food8-img':'food/food11.png'
 
     },
     //cafe12
     {
-        'cafe_name':'', 
+        'cafe_name':'Cute Cups', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink10.png',
+        'food2-img':'drink/drink9.png',
+        'food3-img':'food/food2.png',
+        'food4-img':'drink/drink8.png',
+        'food5-img':'food/food8.png',
+        'food6-img':'drink/drink7.png',
+        'food7-img':'food/food9.png',
+        'food8-img':'food/food10.png'
 
     },
     //cafe13
     {
-        'cafe_name':'', 
+        'cafe_name':'Amooze', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink6.png',
+        'food2-img':'food/food2.png',
+        'food3-img':'drink/drink5.png',
+        'food4-img':'food/food5.png',
+        'food5-img':'food/food6.png',
+        'food6-img':'drink/drink4.png',
+        'food7-img':'food/food15.png',
+        'food8-img':'drink/drink3.png'
 
     },
     //cafe14
     {
-        'cafe_name':'', 
+        'cafe_name':'CHao', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'food/food16.png',
+        'food2-img':'drink/drink2.png',
+        'food3-img':'food/food18.png',
+        'food4-img':'drink/drink1.png',
+        'food5-img':'food/food4.png',
+        'food6-img':'drink/drink10.png',
+        'food7-img':'drink/drink9.png',
+        'food8-img':'food/food3.png'
 
     },
     //cafe15
     {
-        'cafe_name':'', 
+        'cafe_name':'Merp', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink8.png',
+        'food2-img':'food/food2.png',
+        'food3-img':'drink/drink7.png',
+        'food4-img':'food/food6.png',
+        'food5-img':'food/food9.png',
+        'food6-img':'drink/drink6.png',
+        'food7-img':'food/food14.png',
+        'food8-img':'drink/drink5.png'
 
     },
     //cafe16
     {
-        'cafe_name':'', 
+        'cafe_name':'GRE', 
         'header_img':'../image/', 
-        'food1-img':'',
-        'food2-img':'',
-        'food3-img':'',
-        'food4-img':'',
-        'food5-img':'',
-        'food6-img':'',
-        'food7-img':'',
-        'food8-img':''
+        'food1-img':'drink/drink4.png',
+        'food2-img':'drink/drink3.png',
+        'food3-img':'food/food18.png',
+        'food4-img':'drink/drink2.png',
+        'food5-img':'food/food13.png',
+        'food6-img':'drink/drink1.png',
+        'food7-img':'food/food3.png',
+        'food8-img':'food/food5.png'
 
     }   
 ]
