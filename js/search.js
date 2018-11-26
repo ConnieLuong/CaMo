bcfaf0541a3447256bd364b4822f0f20599a29ee
Connie Luong <col032@ucsd.edu>
2018-11-26 13:06:02 -0800

    created search function

--- js/search.js
+++ js/search.js
@@ -1,3 +1,247 @@
+var hashtag_Data = [
+    {
+        'category_id':'quiet',
+        'cafes': [1,4,7,3,12,9,10,6]
+    },
+    {
+        'category_id':'twentyfourh',
+        'cafes': [13,2,14,12,5,3,10,4]
+    },
+    {
+        'category_id':'group',
+        'cafes': [6,2,8,15,12,16,11,9]
+    },
+    {
+        'category_id':'aesthetic',
+        'cafes': [16,10,8,15,13,3,6,11]
+    },
+    {
+        'category_id':'brunch',
+        'cafes': [10,2,7,4,12,9,8,13]
+    },
+    {
+        'category_id':'themed',
+        'cafes': [2,1,5,8,15,11,14,10]
+    },
+    {
+        'category_id':'pet-friendly',
+        'cafes': [1,7,4,3,5,16,13,9]
+    },
+    {
+        'category_id':'vegan',
+        'cafes': [15,14,1,5,16,7,6,11]
+    }
+]
+
+var cafe_card_Data=[
+    //Cafe 1  - The Bean Palace
+    { 
+        'cafe-card-id':'The-Bean-Palace-',
+        'cafe-link':'cafe_page=The-Bean-Palace',
+        'cafe-img':'Cafe1.png',
+        'cafe-name':'The Bean Palace',
+        'cafe-rating':'3.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'+
+            '<img src="../image/tag-themed.png" class="tag-t">'
+    },
+    //Cafe 2 - Lava Java
+    {
+        'cafe-card-id':'Lava-Java-',
+        'cafe link':'cafe_page=Lava-Java',
+        'cafe-img':'Cafe2.png',
+        'cafe-name':'Lava Java',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-24hr.png" class="tag-24">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'+
+            '<img src="../image/tag-group.png" class="tag-g">'+
+            '<img src="../image/tag-themed.png" class="tag-t">'
+    },
+    //Cafe 3 - The Grind
+    {
+        'cafe-card-id':'The-Grind-',
+        'cafe-link':'cafe_page=The-Grind',
+        'cafe-img':'Cafe3.png',
+        'cafe-name':'The Grind',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-aesthetic.png" class="tag-a">'+
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-24hr.png" class="tag-24">'
+    },
+    //Cafe 4 - No Social Life
+    {
+        'cafe-card-id':'No-Social-Life-',
+        'cafe-link':'cafe_page=No-Social-Life',
+        'cafe-img':'Cafe4.png',
+        'cafe-name':'No Social Life',
+        'cafe-rating':'3.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'+
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-24hr.png" class="tag-24">'
+    },
+    //Cafe 5 - No Doze Cafe
+    {
+        'cafe-card-id':'No-Doze-Cafe-',
+        'cafe-link':'cafe_page=No-Doze-Cafe',
+        'cafe-img':'Cafe5.png',
+        'cafe-name':'No Doze Cafe',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-24hr.png" class="tag-24">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'+
+            '<img src="../image/tag-themed.png" class="tag-t">'
+    },
+    //Cafe 6 - Aroma Mocha
+    {
+        'cafe-card-id':'Aroma-Mocha-',
+        'cafe-link':'cafe_page=Aroma-Mocha',
+        'cafe-img':'Cafe6.png',
+        'cafe-name':'Aroma Mocha',
+        'cafe-rating':'5.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-aesthetic.png" class="tag-a">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'+
+            '<img src="../image/tag-group.png" class="tag-g">'
+    },
+    //Cafe 7 - The Split Bean
+    {
+        'cafe-card-id':'The-Split-Bean-',
+        'cafe-link':'cafe_page=The-Split-Bean',
+        'cafe-img':'Cafe7.png',
+        'cafe-name':'The Split Bean',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'
+    },
+    //Cafe 8 - Happy Orange
+    {
+        'cafe-card-id':'Happy-Orange-',
+        'cafe-link':'cafe_page=Happy-Orange',
+        'cafe-img':'Cafe8.png',
+        'cafe-name':'Happy Orange',
+        'cafe-rating':'3.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-aesthetic.png" class="tag-a">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'+
+            '<img src="../image/tag-group.png" class="tag-g">'+
+            '<img src="../image/tag-themed.png" class="tag-t">'
+    },
+    //Cafe 9 - Black Sugars
+    {
+        'cafe-card-id':'Black-Sugars-',
+        'cafe-link':'cafe_page=Black-Sugars',
+        'cafe-img':'Cafe9.png',
+        'cafe-name':'Black Sugars',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'+
+            '<img src="../image/tag-group.png" class="tag-g">'
+    },
+    //Cafe 10 - Bees
+    {
+        'cafe-card-id':'Bees-',
+        'cafe-link':'cafe_page=Bees',
+        'cafe-img':'Cafe10.png',
+        'cafe-name':'Bees',
+        'cafe-rating':'5.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'+
+            '<img src="../image/tag-24hr.png" class="tag-24">'+
+            '<img src="../image/tag-themed.png" class="tag-t">'
+    },
+    //Cafe 11 - QnA
+    {
+        'cafe-card-id':'QnA-',
+        'cafe-link':'cafe_page=QnA',
+        'cafe-img':'Cafe11.png',
+        'cafe-name':'QnA',
+        'cafe-rating':'3.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-aesthetic.png" class="tag-a">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'+
+            '<img src="../image/tag-group.png" class="tag-g">'+
+            '<img src="../image/tag-themed.png" class="tag-t">'
+    },
+    //Cafe 12 - Cute Cups
+    {
+        'cafe-card-id':'Cute-Cups-',
+        'cafe-link':'cafe_page=Cute-Cupts',
+        'cafe-img':'Cafe12.png',
+        'cafe-name':'Cute Cups',
+        'cafe-rating':'3.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-quiet.png" class="tag-q">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'+
+            '<img src="../image/tag-group.png" class="tag-g">'+
+            '<img src="../image/tag-24hr.png" class="tag-24">'
+    },
+    //Cafe 13 - Amooze
+    {
+        'cafe-card-id':'Amooze-',
+        'cafe-link':'cafe_page=Amooze',
+        'cafe-img':'Cafe13.png',
+        'cafe-name':'Amooze',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-aesthetic.png" class="tag-a">'+
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-brunch.png" class="tag-b">'+
+            '<img src="../image/tag-24hr.png" class="tag-24">'
+    },
+    //Cafe 14 - CHao
+    {
+        'cafe-card-id':'CHao-',
+        'cafe-link':'cafe_page=CHao',
+        'cafe-img':'Cafe14.png',
+        'cafe-name':'CHao',
+        'cafe-rating':'3.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-themed.png" class="tag-t">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'+
+            '<img src="../image/tag-24hr.png" class="tag-24">'
+    },
+    //Cafe 15 - Merp
+    {
+        'cafe-card-id':'Merp-',
+        'cafe-link':'cafe_page=Merp',
+        'cafe-img':'Cafe15.png',
+        'cafe-name':'Merp',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-aesthetic.png" class="tag-a">'+
+            '<img src="../image/tag-group.png" class="tag-g">'+
+            '<img src="../image/tag-themed.png" class="tag-t">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'
+    },
+    //Cafe 16 - GRE
+    {   
+        'cafe-card-id':'GRE-',
+        'cafe-link':'cafe_page=GRE',
+        'cafe-img':'Cafe16.png',
+        'cafe-name':'GRE',
+        'cafe-rating':'4.png',
+        'cafe-hashtags':
+            '<img src="../image/tag-pet.png" class="tag-p">'+
+            '<img src="../image/tag-vegan.png" class="tag-v">'+
+            '<img src="../image/tag-group.png" class="tag-g">'+
+            '<img src="../image/tag-aesthetic.png" class="tag-a">'
+    }
+];
 /**
  * Description: Given keyword, will conduct search:
  *      * If keyword matches a category, will load all cafes under that category
@@ -38,15 +282,15 @@ function search(keyword){
  * @param {string} keyword 
  * 
  * Output:
- *      list - [1,{dictionary containing cafes for specific category}]
+ *      list - [1,,,,]
  *      null if nothing found
  */
 function hashtags_search(keyword){
     var list = null;
-    for(var i=0; i<categoryData.length; i++){
-        if(keyword==categoryData[i]["category_id"]){
-            list.append(1);
-            list.append(categoryData[i]);
+    for(var i=0; i<hashtag_Data.length; i++){
+        if(keyword==hashtag_Data[i]["category_id"]){
+            var arr = [1];
+            list = arr.concat(hashtag_Data[i]['cafes']);
         }
     }
     return list;
@@ -64,8 +308,8 @@ function cafe_search(keyword){
     var list = null;
     for(var i=0; i<cafe_card_Data.length; i++){
         if(keyword==cafe_card_Data[i]["cafe-name"]){
-            list.append(2);
-            list.append(cafe_card_Data[i]);
+            arr = [2];
+            list = arr.concat([i]);
         }
     }
     return list;
--- js/searchResults.js
+++ js/searchResults.js
@@ -14,27 +14,89 @@ $(document).ready(function(){
 
     //allow for fake-database queries
     var queryParams = new URLSearchParams(window.location.search);
-    var keyword = queryParams.get('cafe_page');
+    var keyword = queryParams.get('keyword');
+    document.getElementById('keyword').innerHTML = keyword;
+    document.getElementById('search-input').value = keyword;
     console.log('query for', keyword);
 
     var list = search(keyword);
+    console.log('list = ', list);
     var success = list[0];
     //do something with the success value
     if(success==0){
+        console.log("Success = 0.");
+
+
         parentDiv.append('<h3 class="col-sm-12">No matches found.</h3><h2 class="col-sm-12">More Cafes to Consider<h2>');
         //parse remaining cafe indices --> grab cafe info by index from cafe_card_Data
-    }else if(success==1){
-        //parse where list[1] contains cafe1, cafe2, cafe3... to match template cafeCard
-    }else if(success==2){
-        //parse where list[1] contains cafe info matching cafeCard
-    }
+        for(var i=1; i<list.length; i++){
+            //since the value at each index in list is the cafe's corresponding #, 
+            var cafe = list[i]-1;
+            var curData = cafe_card_Data[cafe];
+            var curHtml = template(curData);
 
-    for(var i=1; i<list.length; i++){
-        //since the value at each index in list is the cafe's corresponding #, 
-        // TODO will need to change parsing method
-        var cafe = list[i]
+            console.log('curHTML', curHTML);
+
+            parentDiv.append(curHtml);
+        }
+    }else if(success==1){
+        console.log("Success = 1.", list[1]);
+
+
+        //list = [1,1,2,3,4,5,6,7,8]
+        //parse where list[1] contains [cafe1, cafe2, cafe3...] to match template cafeCard
+        for(var i=1; i<list.length; i++){
+            //since the value at each index in list is the cafe's corresponding #, 
+            var cafe = list[i]-1;
+            var curData = cafe_card_Data[cafe];
+            var curHtml = template(curData);
+
+            console.log('curHTML', curHTML);
+
+            parentDiv.append(curHtml);
+        }
+    }else if(success==2){
+        console.log("Success = 2.");
+
+        //list = [2,#]
+        //parse where list[1] contains cafe info matching cafeCard
+        var cafe = list[1];
+        console.log('cafe = ', cafe);
+
         var curData = cafe_card_Data[cafe];
-        var curHtml = template(curData);
-        parentDiv.append(curHtml);
+
+        console.log('curData = ', curData);
+
+        var curHTML = template(curData);
+
+        console.log('curHTML', curHTML);
+
+        parentDiv.append(curHTML);
     }
 })
+
+function linkToSearch(){
+    var keyword = document.getElementById('search-input').value;
+    var loc = "searchResults.html?keyword="+keyword;
+    window.location = (loc);
+}
+
+// hover effect for search icon
+function searchHover(element) {
+    element.setAttribute('src', '../image/searchHover.png');
+}
+
+function searchUnhover(element) {
+    element.setAttribute('src', '../image/searchIcon.png');
+}
+
+//keydown
+var searchElem = document.getElementById('search-input');
+searchElem.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+        var keyword = document.getElementById('search-input').value;
+        var loc = "searchResults.html?keyword="+keyword;
+        window.location = (loc);
+    }  
+});
